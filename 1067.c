58536601a265ab719f274751371e4322b84492e4
Muhammad Shahriar Islam <140098840+Shahriar-Shezan@users.noreply.github.com>
2023-08-03 12:46:23 +0600

    Add files via upload

---
+++ 1067.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main ()
+{
+    int i, count, n;
+    scanf("%d", &n);
+    i = 1;
+    while (i <= n)
+    {
+        if(i % 2 == 1)
+        {
+            printf("%d\n", i);
+        }
+        i++;
+    }
+    return 0;
+}
+
