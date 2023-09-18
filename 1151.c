893f6ef6bf625b4beda9f4a7d9925c35fb0b711c
Muhammad Shahriar Islam <140098840+Shahriar-Shezan@users.noreply.github.com>
2023-09-19 00:47:36 +0600

    Add files via upload

---
+++ 1151.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main ()
+{
+    int i, n1, n2, n3, n;
+    i = 1;
+    scanf("%d", &n);
+    n1 = 0;
+    n2 = 1;
+    printf("%d %d", n1, n2);
+    while (i < n-1)
+    {
+        n3 = n1+n2;
+        printf(" %d", n3);
+        n1 = n2;
+        n2 = n3;
+        i++;
+    }
+    printf("\n");
+    return 0;
+}
