fadc22bd37cff74ee40935829dffa3edb766af47
Muhammad Shahriar Islam <140098840+Shahriar-Shezan@users.noreply.github.com>
2023-09-20 09:09:16 +0600

    Add files via upload

---
+++ Pattern_printing.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main ()
+{
+    /*int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }*/
+
+    /*int n,m,i,j;
+    scanf("%d%d",&n,&m);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=m;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }*/
+
+    /*int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }*/
+
+    /*int n,i,j,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        for(j=n-1;j>=i;j--)
+        {
+            printf(" ");
+        }
+        for(k=1;k<=i;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }*/
+
+    /*int n,i,j,k;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++,k=0)
+    {
+        for(j=1;j<=n-i;j++)
+        {
+            printf(" ");
+        }
+        for(;k!=2*i-1;k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }*/
+
+
+
+}
